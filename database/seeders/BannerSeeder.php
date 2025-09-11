80dd1a723681571f4191c59f64e9bb9b19a192db
vasil maxim <your.email@example.com>
2025-09-11 18:14:17 +0300

    картинки работают

--- database/seeders/BannerSeeder.php
+++ database/seeders/BannerSeeder.php
@@ -12,26 +12,26 @@ class BannerSeeder extends Seeder
         Banner::create([
             'title' => 'Скидка 120% на все товары для студентов IThub',
             'description' => 'Только этой семестр получите скидку 120% на весь проезд',
-            'image_url' => 'https://vizitspb.ru/wp-content/uploads/2025/05/podorozhnik-terminal.jpg',
-            'link_url' => '/promo/sale20',
+            'image_url' => 'images/podorozhnik-terminal.jpg',
+            'link_url' => 'https://ithub.ru/',
             'position' => 1,
             'is_active' => true
         ]);
 
         Banner::create([
-            'title' => 'Бесплатная доставка',
-            'description' => 'Бесплатная доставка при заказе от 5000 рублей',
-            'image_url' => 'https://example.com/banner2.jpg',
-            'link_url' => '/delivery',
+            'title' => 'Комару сидит на БАНКАХ???',
+            'description' => 'это просто шок контент',
+            'image_url' => 'images/komaru.jpg',
+            'link_url' => 'https://ru.pinterest.com/2komaru2/%D0%BA%D0%BE%D0%BC%D0%B0%D1%80%D1%83/',
             'position' => 2,
             'is_active' => true
         ]);
 
         Banner::create([
-            'title' => 'Новая коллекция',
-            'description' => 'Ознакомьтесь с новой осенней коллекцией',
-            'image_url' => 'https://example.com/banner3.jpg',
-            'link_url' => '/new-collection',
+            'title' => 'хочешь быть таким как он?',
+            'description' => 'а сколько шариков лопнул ты?',
+            'image_url' => 'images/btd.png',
+            'link_url' => 'https://ru.wikipedia.org/wiki/Bloons_Tower_Defense',
             'position' => 3,
             'is_active' => true
         ]);
